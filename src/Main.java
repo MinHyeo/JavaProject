0e767460c8409807ee740228408cef062363f14a
MinHyeo <p610222@naver.com>
2024-06-12 08:25:51 +0900

    Add Input Number
    숫자 입력 구현

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,24 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.util.ArrayList;
 
 class Player{
+    public ArrayList<Integer> numbers;
 
+    public Player(){
+        numbers = new ArrayList<Integer>();
+    }
 }
 
 class GameFrame extends JFrame{
-    public GameFrame(){
+    Player[] player = new Player[2];
+    int playerTurn = 0;
+
+    public GameFrame(Player playerA, Player playerB){
+        player[0] = playerA;
+        player[1] = playerB;
+
         setTitle("숫자 야구");
         setSize(1000, 800);
         setVisible(true);
@@ -40,8 +51,18 @@ class GameFrame extends JFrame{
             @Override
             public void keyTyped(KeyEvent e) {
                 JTextField src = (JTextField)e.getSource();
+                //길이 체크
                 if(src.getText().length() >= 4)
                     e.consume();
+
+                //숫자만 있는지 체크
+                char c = e.getKeyChar();
+                if (!((c >= '0') && (c <= '9') ||
+                        (c == KeyEvent.VK_BACK_SPACE) ||
+                        (c == KeyEvent.VK_DELETE))) {
+                    getToolkit().beep();
+                    e.consume();
+                }
             }
         });
 
@@ -55,14 +76,18 @@ class GameFrame extends JFrame{
             public void actionPerformed(ActionEvent e) {
                 JButton button = (JButton)e.getSource();
                 JFrame frame = (JFrame)button.getTopLevelAncestor();
-                // 기능 추가
+
+                //플레이어 턴 확인 및
+                int number = Integer.valueOf(inputBox.getText());
+                player[playerTurn].numbers.add(number);
+                playerTurn = playerTurn + 1 >= 2 ? 0 : playerTurn + 1;
+                System.out.println(player[0].numbers);
             }
         });
     }
     void PrintPlayerBox(){
         Container c = getContentPane();
         c.setLayout(new BorderLayout());
-
     }
 }
 
@@ -70,7 +95,7 @@ public class Main{
     public static void main(String[] args){
         Player playerA = new Player();
         Player playerB = new Player();
-        GameFrame frame = new GameFrame();
+        GameFrame frame = new GameFrame(playerA, playerB);
 
     }
 }
